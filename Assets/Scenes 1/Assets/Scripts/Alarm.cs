f24d9d9074e0d55e922b3ae9e19c8a3e2ec57672
Chernikova <104696269+EkaterinaChernikova@users.noreply.github.com>
2024-02-17 16:41:43 +0300

    Initial commit

---
+++ Assets/Scenes 1/Assets/Scripts/Alarm.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private float _volumeChangeSpeed = 0.5f;
+    [SerializeField] private AudioSource _audioSource;
+
+    private bool _isVolumeUp;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Movement>(out Movement component) == true)
+        {
+            StartAlarm();
+        }
+    }
+
+    private IEnumerator ChangeVolume()
+    {
+        if (_audioSource.isPlaying == false)
+        {
+            _audioSource.Play();
+        }
+
+        do
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, 
+                _isVolumeUp ? _audioSource.maxDistance : 0, 
+                _volumeChangeSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+        while (_audioSource.volume < _audioSource.maxDistance && _audioSource.volume > 0);
+
+        if(_audioSource.volume == 0)
+        {
+            _audioSource.Stop();
+        }
+
+    }
+
+    public void StartAlarm()
+    {
+        Debug.Log("ALARM");
+        _isVolumeUp = !_isVolumeUp;
+        StartCoroutine(ChangeVolume());
+    }
+}
---
+++ Assets/Scenes 1/Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Movement : MonoBehaviour
+{
+    private const string Speed = "Speed";
+
+    [SerializeField] private float _speed = 10;
+    [SerializeField] private float _rotationSpeed = 240;
+
+    private Animator _animator;
+    private float _animationSwitch = 0.0f;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) 
+            || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            _animationSwitch = 0.05f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * _rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * _rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W) == false && Input.GetKey(KeyCode.S) == false
+            && Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.D) == false)
+        {
+            _animationSwitch = 0.0f;
+        }
+
+        _animator.SetFloat(Speed, _animationSwitch);
+    }
+}
